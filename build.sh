5c0ee8bf20cc268651bb29f18419c614a390d617
afnan <afnanqasim74@gmail.com>
2023-08-16 20:18:24 +0500

    update

--- build.sh
+++ build.sh
@@ -1,2 +1,16 @@
-pip install -r requirements.txt
+chmod +x build.sh
+pip install asgiref==3.2.10
+pip install certifi==2020.6.20
+pip install dj-database-url==0.5.0
+pip install Django==3.1.1
+pip install django-crispy-forms==1.9.2
+pip install gunicorn==20.0.4
+pip install pafy==0.5.5
+pip install pytz==2020.1
+pip install sqlparse==0.3.1
+pip install typing_extensions==4.7.1
+pip install tzdata==2023.3
+pip install whitenoise==5.2.0
+pip install youtube-dl==2020.12.14
+
 python3 manage.py runserver
